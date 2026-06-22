323bb36a90bc590a86d30f2618dd7ac81546b5df
goncalvesoliveirafabricio-ceep <goncalves.oliveira.fabricio@escola.pr.gov.br>
2026-06-21 22:44:58 -0300

    Ocorrencias JS Listagem 21-06-2026
    Ocorrencias JS Listagem 21-06-2026

--- telas/ocorrencias1.js
+++ telas/ocorrencias1.js
@@ -1,7 +1,7 @@
 // =========================================================================
 // CONFIGURAÇÕES GLOBAIS E CONTROLE DE ESTADO
 // =========================================================================
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://qcsoftware2.onrender.com";
 
 const ITENS_POR_PAGINA = 10;
 let paginaAtualOcorrencias = 1;  
